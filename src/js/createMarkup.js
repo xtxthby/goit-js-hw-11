c9320417d7c690d872986a11897660cee2f52362
Yurii <116266510+xtxthby@users.noreply.github.com>
2023-03-19 19:17:23 +0200

    add corrections

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -9,37 +9,37 @@ function createPhotoMarkup(searchQuery) {
       comments,
       downloads,
     }) => `<div class="photo-card">
-    <a class='gallery__link' href='${largeImageURL}'>
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width='400px' />
-  </a>
-  <div class="info">
-    <div>
-      <p class="info-item">
-        <b>Likes</b>
-      <p class="info-item">${likes}</p>
-      </p>
-    </div>
-    <div>
-      <p class="info-item">
-        <b>Views</b>
-      </p>
-      <p class="info-item">${views}</p>
-    </div>
-    <div>
-      <p class="info-item">
-        <b>Comments</b>
-        <p class="info-item">${comments}</p>
-      </p>
-    </div>
-    <div>
-      <p class="info-item">
-        <b>Downloads</b>
-        <p class="info-item">${downloads}</p>
-      </p>
-    </div>
-  </div>
-</div>`
-  );
+      <a class='gallery__link' href='${largeImageURL}'>
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" width='400px' />
+      </a>
+      <div class="info">
+        <div>
+          <p class="info-item">
+            <b>Likes</b>
+            <p class="info-item">${likes}</p>
+          </p>
+        </div>
+        <div>
+          <p class="info-item">
+            <b>Views</b>
+          </p>
+          <p class="info-item">${views}</p>
+        </div>
+        <div>
+          <p class="info-item">
+            <b>Comments</b>
+            <p class="info-item">${comments}</p>
+          </p>
+        </div>
+        <div>
+          <p class="info-item">
+            <b>Downloads</b>
+            <p class="info-item">${downloads}</p>
+          </p>
+        </div>
+      </div>
+    </div>`
+  ).join('');
 }
 
 export { createPhotoMarkup };
